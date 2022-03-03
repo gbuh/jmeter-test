2937385a004b363276bd15b9697de98a1032b9c0
Ihar Lipko <ihar.lipko@ukg.com>
2022-03-03 18:14:23 +0100

    update buildspec.

--- launch.sh
+++ launch.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-apt-get update
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+# apt-get update
+# apt-get install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get install docker-ce docker-ce-cli containerd.io
+# apt-get install docker-ce docker-ce-cli containerd.io
 # apt install docker-ce docker-ce-cli containerd.io
 # systemctl start docker
 # systemctl enable docker
-docker --version
+# docker --version
 #!/bin/bash
 #
 # This script expects the standdard JMeter command parameters.

82d315f08a779d4d5f5e8e812ec63e0484b262a7
Ihar Lipko <ihar.lipko@ukg.com>
2022-03-03 18:10:04 +0100

    update

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ apt-get install \
     lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null

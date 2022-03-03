7dd6f710e24b5ca48f8e8cdc6c2d7028301b9135
Ihar Lipko <ihar.lipko@ukg.com>
2022-03-03 16:41:59 +0100

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
-# sudo apt-get remove docker docker-engine docker.io
-apt install docker-ce docker-ce-cli containerd.io
-systemctl start docker
-systemctl enable docker
+apt-get update
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+# apt install docker-ce docker-ce-cli containerd.io
+# systemctl start docker
+# systemctl enable docker
 docker --version
 #!/bin/bash
 #
